27bb57d2f9a3048d0df21eb7149948a95ee158f2
Arman <arman.gh45@gmail.com>
2015-05-16 20:36:00 -0400

    added adapter

---
+++ app/src/main/java/com/example/armanghassemi/liveupdate/StatusAdapter.java
@@ -0,0 +1,11 @@
+package com.example.armanghassemi.liveupdate;
+
+import android.widget.ArrayAdapter;
+
+import com.parse.ParseObject;
+
+/**
+ * Created by armanghassemi on 2015-05-16.
+ */
+public class StatusAdapter extends ArrayAdapter<ParseObject> {
+}
